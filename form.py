99a74f6e32dd621bc554f2615b9a9b468e5fa9b1
alisadaintanvir <alisadaintanvir@gmail.com>
2023-10-14 05:06:51 +0600

    implement a ranking feature so no movie will be added with the same ranking

--- form.py
+++ form.py
@@ -5,17 +5,22 @@ from wtforms.validators import DataRequired, NumberRange, URL
 
 class MovieForm(FlaskForm):
     title = StringField('Movie Title', validators=[DataRequired()])
-    # year = IntegerField('Year')
-    # description = TextAreaField('Description')
-    # rating = FloatField('Rating', validators=[NumberRange(min=0, max=10)])
-    # ranking = IntegerField('Ranking', validators=[NumberRange(min=1)])
-    # review = TextAreaField('Review')
-    # img_url = StringField('Image URL', validators=[URL()])
+    year = IntegerField('Year')
+    description = TextAreaField('Description')
+    rating = FloatField('Rating', validators=[NumberRange(min=0, max=10)])
+    ranking = IntegerField('Ranking', validators=[NumberRange(min=1)])
+    review = TextAreaField('Review')
+    img_url = StringField('Image URL', validators=[URL()])
+    submit = SubmitField("Add")
+
+
+class FindMovieForm(FlaskForm):
+    title = StringField('Movie Title', validators=[DataRequired()])
     submit = SubmitField("Add")
 
 
 class EditMovieForm(FlaskForm):
+    ranking = IntegerField('Ranking', validators=[NumberRange(min=1, max=10)])
     rating = FloatField('Rating', validators=[NumberRange(min=0, max=10)])
-    id = HiddenField('ID')
     review = TextAreaField('Review')
     submit = SubmitField("Update")
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 import requests
 from flask_migrate import Migrate
 from db import db, Movie
-from form import MovieForm, EditMovieForm
+from form import MovieForm, EditMovieForm, FindMovieForm
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("APP_SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
 Bootstrap(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
 session = db.session
+
+TMDB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+TMDB_INFO_URL = "https://api.themoviedb.org/3/movie"
+TMDB_IMAGE_URL = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2"
+TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
 
 
 @app.route("/")
@@ -23,27 +32,33 @@ def home():
 
 @app.route('/add', methods=["GET", "POST"])
 def add():
-    form = MovieForm()
+    form = FindMovieForm()
     if request.method == 'POST' and form.validate_on_submit():
-        url = f"https://api.themoviedb.org/3/search/movie?query={form.title.data}&include_adult=false&page=1"
-        api_key = "68c20e692786348a944bebd10aaa9c4c"
-        response = requests.get(url, params={"api_key": api_key, "query": form.title.data})
+        response = requests.get(TMDB_SEARCH_URL, params={"api_key": TMDB_API_KEY, "language": "en-US",
+                                                         "query": form.title.data})
         data = response.json()['results']
-        print(data)
-        # new_movie = Movie(
-        #     title=form.title.data,
-        #     # year=form.year.data,
-        #     # description=form.description.data,
-        #     # rating=form.rating.data,
-        #     # ranking=form.ranking.data,
-        #     # review=form.review.data,
-        #     # img_url=form.img_url.data
-        # )
-        # session.add(new_movie)
-        # session.commit()
         return render_template('select.html', options=data)
 
     return render_template('add.html', form=form)
+
+
+@app.route('/find')
+def find():
+    movie_id = request.args.get('id')
+    if movie_id:
+        movie_api_url = f"{TMDB_INFO_URL}/{movie_id}"
+        response = requests.get(movie_api_url, params={"api_key": TMDB_API_KEY, "language": "en-US"})
+        data = response.json()
+        print(data)
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split('-')[0],
+            description=data["overview"],
+            img_url=f"{TMDB_IMAGE_URL}/{data['poster_path']}"
+        )
+        session.add(new_movie)
+        session.commit()
+        return redirect(url_for('edit', id=new_movie.id))
 
 
 @app.route('/edit', methods=['GET', 'POST'])
@@ -51,11 +66,18 @@ def edit():
     movie_id = request.args.get('id')
     movie_selected = session.get(Movie, movie_id)
     form = EditMovieForm(obj=movie_selected)
+
     if request.method == 'POST' and form.validate_on_submit():
-        movie_selected.rating = form.rating.data
-        movie_selected.review = form.review.data
-        session.commit()
-        return redirect(url_for('home'))
+        existing_movie = session.query(Movie).filter(Movie.ranking == form.ranking.data).first()
+        if existing_movie and existing_movie != movie_selected:
+            flash(f"A movie name {existing_movie.title} with the same ranking already exists. "
+                  f"Please change the ranking.", 'error')
+        else:
+            movie_selected.ranking = form.ranking.data
+            movie_selected.rating = form.rating.data
+            movie_selected.review = form.review.data
+            session.commit()
+            return redirect(url_for('home'))
     return render_template('edit.html', form=form, movie=movie_selected)
 
 
