e4352e16440c23f8cef6858b6a60ba0fae5f68b9
alisadaintanvir <alisadaintanvir@gmail.com>
2023-10-13 21:32:26 +0600

    implement movie search feature by title from TMDB api

--- form.py
+++ form.py
@@ -4,13 +4,13 @@ from wtforms.validators import DataRequired, NumberRange, URL
 
 
 class MovieForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    year = IntegerField('Year')
-    description = TextAreaField('Description')
-    rating = FloatField('Rating', validators=[NumberRange(min=0, max=10)])
-    ranking = IntegerField('Ranking', validators=[NumberRange(min=1)])
-    review = TextAreaField('Review')
-    img_url = StringField('Image URL', validators=[URL()])
+    title = StringField('Movie Title', validators=[DataRequired()])
+    # year = IntegerField('Year')
+    # description = TextAreaField('Description')
+    # rating = FloatField('Rating', validators=[NumberRange(min=0, max=10)])
+    # ranking = IntegerField('Ranking', validators=[NumberRange(min=1)])
+    # review = TextAreaField('Review')
+    # img_url = StringField('Image URL', validators=[URL()])
     submit = SubmitField("Add")
 
 
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from db import db, Movie
 from form import MovieForm, EditMovieForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
 Bootstrap(app)
 
@@ -17,25 +17,31 @@ session = db.session
 
 @app.route("/")
 def home():
-    return render_template("index.html", movies=Movie.query.all())
+    movies = Movie.query.order_by(Movie.ranking.desc()).all()
+    return render_template("index.html", movies=movies)
 
 
 @app.route('/add', methods=["GET", "POST"])
 def add():
     form = MovieForm()
-    if request.method == 'POST':
-        new_movie = Movie(
-            title=form.title.data,
-            year=form.year.data,
-            description=form.description.data,
-            rating=form.rating.data,
-            ranking=form.ranking.data,
-            review=form.review.data,
-            img_url=form.img_url.data
-        )
-        session.add(new_movie)
-        session.commit()
-        return redirect(url_for('home'))
+    if request.method == 'POST' and form.validate_on_submit():
+        url = f"https://api.themoviedb.org/3/search/movie?query={form.title.data}&include_adult=false&page=1"
+        api_key = "68c20e692786348a944bebd10aaa9c4c"
+        response = requests.get(url, params={"api_key": api_key, "query": form.title.data})
+        data = response.json()['results']
+        print(data)
+        # new_movie = Movie(
+        #     title=form.title.data,
+        #     # year=form.year.data,
+        #     # description=form.description.data,
+        #     # rating=form.rating.data,
+        #     # ranking=form.ranking.data,
+        #     # review=form.review.data,
+        #     # img_url=form.img_url.data
+        # )
+        # session.add(new_movie)
+        # session.commit()
+        return render_template('select.html', options=data)
 
     return render_template('add.html', form=form)
 
@@ -53,14 +59,14 @@ def edit():
     return render_template('edit.html', form=form, movie=movie_selected)
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    movie_to_delete = Movie.query.get(id)
+@app.route('/delete')
+def delete():
+    movie_id = request.args.get('id')
+    movie_to_delete = session.get(Movie, movie_id)
     if movie_to_delete:
         session.delete(movie_to_delete)
         session.commit()
     return redirect(url_for('home'))
-
 
 
 if __name__ == '__main__':
